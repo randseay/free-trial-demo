5ba331ae98cff32eeecd6c6f786993c0a3ea50bd
Rand Seay <rand.seay@gmail.com>
2022-09-13 16:06:10 -0700

    Add Heroicons to the project.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,12 @@
 import mark from "./mark.svg";
+import { Bars3Icon } from "@heroicons/react/24/solid";
 
 function Header() {
   return (
     <div className="flex pt-6 justify-between">
       <img src={mark} alt="logo" />
-      <p>Menu</p>
+
+      <Bars3Icon className="gray-400" />
     </div>
   );
 }
