927ad55f530bcf5537c0a075d0500fd8827d0848
Rand Seay <rand.seay@gmail.com>
2022-09-13 16:08:06 -0700

    Add prettier tailwind plugin.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,7 +3,7 @@ import { Bars3Icon } from "@heroicons/react/24/solid";
 
 function Header() {
   return (
-    <div className="flex pt-6 justify-between">
+    <div className="flex justify-between pt-6">
       <img src={mark} alt="logo" />
 
       <Bars3Icon className="gray-400" />
