2ddf8e2234b0d4876ff45fab383011149087caa9
Rand Seay <rand.seay@gmail.com>
2022-09-13 21:12:55 -0700

    First pass at styling the button.

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,5 +1,12 @@
-function Button() {
-  return <button>Button</button>;
+function Button({ children, ...props }) {
+  return (
+    <button
+      className="w-full rounded-md bg-indigo-500 px-4 py-3 text-base font-medium leading-6 shadow"
+      {...props}
+    >
+      {children}
+    </button>
+  );
 }
 
 export default Button;
