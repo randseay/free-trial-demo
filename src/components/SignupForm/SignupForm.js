29f6c59a7b07705a165b10980779fd51da92b1f5
Rand Seay <rand.seay@gmail.com>
2022-09-13 21:13:17 -0700

    Set up the signup form.

--- src/components/SignupForm/SignupForm.js
+++ src/components/SignupForm/SignupForm.js
@@ -1,23 +1,36 @@
+import { useCallback, useState } from "react";
+
 import Button from "components/Button";
 
 function SignupForm() {
+  const [email, setEmail] = useState("");
+
+  const submit = useCallback(
+    (e) => {
+      e.preventDefault();
+      console.log(email);
+    },
+    [email]
+  );
+
   return (
-    <form className="flex flex-col gap-y-3">
+    <form className="flex flex-col gap-y-3" onSubmit={submit}>
       <label htmlFor="email">
         <input
           className="w-full rounded-md px-4 py-3 text-base font-normal leading-6 text-gray-500"
           placeholder="Enter your email"
           id="email"
           name="email"
+          onChange={(e) => setEmail(e.target.value)}
         />
       </label>
 
-      <Button>Start free trial</Button>
+      <Button type="submit">Start free trial</Button>
 
-      <p className="text-sm font-normal leading-5">
+      <p className="text-sm font-normal leading-5 text-gray-300">
         Start your free 14-day trial, no credit card necessary. By providing
         your email, you agree to our{" "}
-        <a href="#" className="font-medium">
+        <a href="#" className="font-medium text-white hover:underline">
           terms or service
         </a>
         .
