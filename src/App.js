ded6967bfce3ae80f689f49f67ae293dc0660bbe
Rand Seay <rand.seay@gmail.com>
2022-09-13 22:29:08 -0700

    Responsive styles.

--- src/App.js
+++ src/App.js
@@ -7,27 +7,31 @@ import illustration from "./illustration.svg";
 function App() {
   return (
     <div className="min-h-screen bg-gray-900 text-white">
-      <Header />
+      <div className="mx-auto max-w-7xl">
+        <Header />
 
-      <div className="flex flex-col p-5">
-        <HiringBadge />
+        <div className="flex flex-col p-5 sm:mt-32 sm:p-8">
+          <HiringBadge />
 
-        <div className="mt-4">
-          <div className="mb-3 flex flex-col text-4xl font-extrabold leading-10 tracking-tight">
-            <h1 className="text-white">A better way to</h1>
-            <h1 className="text-indigo-400">ship web apps</h1>
+          <div className="mt-4 mb-10 max-w-sm sm:mt-6 sm:mb-12 sm:max-w-md lg:max-w-lg xl:max-w-xl">
+            <div className="mb-3 flex flex-col text-4xl font-extrabold leading-10 tracking-tight sm:mb-5 sm:text-5xl lg:text-6xl xl:text-7xl">
+              <h1 className="text-white">A better way to</h1>
+              <h1 className="text-indigo-400">ship web apps</h1>
+            </div>
+
+            <p className="text-base font-normal leading-6 text-gray-300 sm:text-lg lg:text-xl xl:text-2xl">
+              Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui
+              Lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat
+              fugiat.
+            </p>
           </div>
 
-          <p className="mb-10 text-base font-normal leading-6 text-gray-300">
-            Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui
-            Lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat
-            fugiat.
-          </p>
+          <div className="sm:max-w-lg lg:max-w-xl">
+            <SignupForm />
+          </div>
 
-          <SignupForm />
+          <img src={illustration} alt="illustration" />
         </div>
-
-        <img src={illustration} alt="illustration" />
       </div>
     </div>
   );
--- src/components/SignupForm/SignupForm.js
+++ src/components/SignupForm/SignupForm.js
@@ -14,20 +14,27 @@ function SignupForm() {
   );
 
   return (
-    <form className="flex flex-col gap-y-3" onSubmit={submit}>
-      <label htmlFor="email">
-        <input
-          className="w-full rounded-md px-4 py-3 text-base font-normal leading-6 text-gray-500"
-          placeholder="Enter your email"
-          id="email"
-          name="email"
-          onChange={(e) => setEmail(e.target.value)}
-        />
-      </label>
+    <>
+      <form
+        className="flex w-full flex-col gap-y-3 sm:flex-row sm:gap-x-3"
+        onSubmit={submit}
+      >
+        <label htmlFor="email" className="w-full">
+          <input
+            className="w-full rounded-md px-4 py-3 text-base font-normal leading-6 text-gray-500"
+            placeholder="Enter your email"
+            id="email"
+            name="email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </label>
 
-      <Button type="submit">Start free trial</Button>
+        <div>
+          <Button type="submit">Start free trial</Button>
+        </div>
+      </form>
 
-      <p className="text-sm font-normal leading-5 text-gray-300">
+      <p className="mt-3 text-sm font-normal leading-5 text-gray-300">
         Start your free 14-day trial, no credit card necessary. By providing
         your email, you agree to our{" "}
         <a href="#" className="font-medium text-white hover:underline">
@@ -35,7 +42,7 @@ function SignupForm() {
         </a>
         .
       </p>
-    </form>
+    </>
   );
 }
 
